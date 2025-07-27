51a00eec9d8fecace9a5e90e31b00561af30a9e5
halaluddin2000 <helaluddin7001@gmail.com>
2025-07-27 16:49:28 +0600

    added task

--- src/pages/tasks/AddTask.tsx
+++ src/pages/tasks/AddTask.tsx
@@ -8,23 +8,49 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { Textarea } from "@/components/ui/textarea";
+import { addTask } from "@/redux/features/task/taskSlice";
 import { DialogDescription } from "@radix-ui/react-dialog";
+
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 
 export function AddTaskModel() {
   const form = useForm();
 
+  const dispatch = useDispatch();
+
   const onSubmit = (data) => {
     console.log(data);
+    dispatch(addTask(data));
   };
 
   return (
@@ -41,7 +67,7 @@ export function AddTaskModel() {
             <DialogTitle>Add Task</DialogTitle>
           </DialogHeader>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+            <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control}
                 name="title"
@@ -66,11 +92,84 @@ export function AddTaskModel() {
                   </FormItem>
                 )}
               />
+
+              <FormField
+                control={form.control}
+                name="priority"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Priority</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a Priority display" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="low">Low</SelectItem>
+                        <SelectItem value="medium">Medium</SelectItem>
+                        <SelectItem value="high">High</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              {/* time zone start */}
+              <FormField
+                control={form.control}
+                name="dob"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel>Date of birth</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-[240px] pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, "PPP")
+                            ) : (
+                              <span>Pick a date</span>
+                            )}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          disabled={(date) =>
+                            date > new Date() || date < new Date("1900-01-01")
+                          }
+                          captionLayout="dropdown"
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormDescription>
+                      Your date of birth is used to calculate your age.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* time zone end */}
+
               <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button type="submit">Save changes</Button>
+                <DialogClose asChild></DialogClose>
+                <Button className="mt-5" type="submit">
+                  Save changes
+                </Button>
               </DialogFooter>
             </form>
           </Form>
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,40 +1,38 @@
 import type { RootState } from "@/redux/store";
 import type { ITask } from "@/type";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
 
 interface InitialState {
   tasks: ITask[];
+  filter: "all" | "high" | "low";
 }
 
 const initialState: InitialState = {
-  tasks: [
-    {
-      id: "aljdfjdfsjljfsdljsdfj",
-      title: "Initialize frontend",
-      description: "Create Home page, and routing",
-      dueDate: "2025-11",
-      isCompleted: false,
-      priority: "High",
-    },
-    {
-      id: "aljdfjdljfsdljsdfj",
-      title: "Create github repo",
-      description: "Create state routing",
-      dueDate: "2025-11",
-      isCompleted: false,
-      priority: "Medium",
-    },
-  ],
+  tasks: [],
+  filter: "all",
 };
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
-  reducers: {},
+  reducers: {
+    addTask: (state, action: PayloadAction<ITask>) => {
+      const id = uuidv4();
+      const taskData = {
+        ...action.payload,
+        id,
+        isCompleted: false,
+      };
+      state.tasks.push(taskData);
+    },
+  },
 });
 
 export const selectTasks = (state: RootState) => {
   return state.todo.tasks;
 };
+
+export const { addTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
