ce124fb2ec4533937a9177b41c88af4b903c64ed
halaluddin2000 <helaluddin7001@gmail.com>
2025-08-06 21:02:56 +0600

    filtering all low medium or high

--- src/pages/tasks/Tasks.tsx
+++ src/pages/tasks/Tasks.tsx
@@ -1,17 +1,55 @@
 // import { useAppSelector } from "@/redux/hook";
 
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useSelector } from "react-redux";
+import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
+import { useDispatch, useSelector } from "react-redux";
 import TasksCard from "./TasksCard";
 import { AddTaskModel } from "./AddTask";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Tasks() {
   const tasks = useSelector(selectTasks);
+  const dispatch = useDispatch();
   console.log(tasks);
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl">Tasks</h2>
+      <div className="flex justify-end items-center gap-5">
+        <h2 className="text-2xl mr-auto">Tasks</h2>
+        <Tabs defaultValue="all">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => {
+                dispatch(updateFilter("all"));
+              }}
+              value="all"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => {
+                dispatch(updateFilter("low"));
+              }}
+              value="low"
+            >
+              Low
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => {
+                dispatch(updateFilter("medium"));
+              }}
+              value="medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => {
+                dispatch(updateFilter("high"));
+              }}
+              value="high"
+            >
+              High
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
         <AddTaskModel />
       </div>
       <div className="space-y-5 mt-5">
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 
 interface InitialState {
   tasks: ITask[];
-  filter: "all" | "high" | "low";
+  filter: "all" | "low" | "medium" | "high";
 }
 
 const initialState: InitialState = {
@@ -46,13 +46,26 @@ const taskSlice = createSlice({
     deleteTask: (state, action: PayloadAction<string>) => {
       state.tasks = state.tasks.filter((task) => task.id !== action.payload);
     },
+    updateFilter: (state, action: PayloadAction<"low" | "medium" | "high">) => {
+      state.filter = action.payload;
+    },
   },
 });
 
 export const selectTasks = (state: RootState) => {
+  const filter = state.todo.filter;
+
+  if (filter === "low") {
+    return state.todo.tasks.filter((task) => task.priority === "low");
+  } else if (filter === "medium") {
+    return state.todo.tasks.filter((task) => task.priority === "medium");
+  } else if (filter === "high") {
+    return state.todo.tasks.filter((task) => task.priority === "high");
+  }
   return state.todo.tasks;
 };
 
-export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask, updateFilter } =
+  taskSlice.actions;
 
 export default taskSlice.reducer;
